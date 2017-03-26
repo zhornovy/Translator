3a8ffc5f7d1d425463cb9288b40c53bc63399d26
zhornovy <ed@zhornovy.com>
2017-03-26 20:56:47 +0300

    Created Class Reverse Notation
    Reverse Notation for making Polish Reverse Notation

---
+++ Translator/Pnotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator
+{
+    //Reverse Polish notation
+    class Pnotation
+    {
+        private List<Output> lexemesOutput;
+        public Pnotation(List<Output> LO )
+        {
+            lexemesOutput = LO;
+        }
+    }
+}
